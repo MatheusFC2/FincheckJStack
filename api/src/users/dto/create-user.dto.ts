7ce4cccd02b7556f3ac6f4d30863bf428b92c102
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-11-12 20:37:55 -0300

    feat: idea about mensage

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -9,6 +9,7 @@ export class CreateUserDto {
   @IsEmail()
   email: string;
 
+  // Caso queira alterar o texto quando e feito uma req @IsString({ mensage: "texto em portugues"} )
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
