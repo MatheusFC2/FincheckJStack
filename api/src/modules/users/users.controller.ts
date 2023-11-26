115353666783b06fe69cb3037f5e03da5694c5bf
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-11-26 13:11:50 -0300

    feat: modifying show findAll UserId

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Req, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Request } from 'express';
-import { IsPublic } from 'src/shared/decorators/IsPublic';
 import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 @Controller('users')
